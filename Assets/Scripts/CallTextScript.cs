31c27c92613f764282351f9e3ce04d2f026a8252
Oliver <oliver_marsh@live.com.au>
2019-04-29 21:31:33 +0930

    adding extra room

---
+++ Assets/Scripts/CallTextScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallTextScript : MonoBehaviour
+{
+	public TextWriter textWriter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void CancelFontWriting() {
+    	textWriter.CancelFontWriting();
+    }
+    public void ClearFontWriter() {
+    	textWriter.ClearFontWriter();
+    }
+    public void BeginTextWriting() {
+    	textWriter.ActivateFontWriter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Timer_namespace;
+
+public class TextWriter : MonoBehaviour
+{	
+	private Text textObject;
+	private string startString;
+	private Timer showTimer;
+	private bool showText;
+	private float charTimeLength;
+	private int lastCharCount;
+	public float timeToShow;
+
+	public void ClearFontWriter() {
+		textObject.text = "";
+	}
+
+	public void ActivateFontWriter() {
+		showTimer = new Timer(timeToShow);
+        textObject.text = "";
+        showTimer.turnOn();
+        showText = true;
+        charTimeLength = showTimer.period / (startString.Length);
+        lastCharCount = 0;
+	}
+
+	public void CancelFontWriting() {
+		showText = false;
+    	showTimer.turnOff();
+    	textObject.text = "";
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+    	textObject = gameObject.GetComponent<Text>();
+    	startString = textObject.text;
+    	textObject.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+    	if(showText) {
+    		if(showTimer.isOn()) {
+	    		bool finished = showTimer.updateTimer(Time.fixedDeltaTime);
+	    		int numOfCharacters = (int)(showTimer.tAt / charTimeLength);
+	    		if(numOfCharacters < 0) {
+	    			numOfCharacters = 0;
+	    		}
+	    		if(numOfCharacters != lastCharCount) {
+	    			textObject.text = startString.Substring(0, numOfCharacters);
+	    			lastCharCount = numOfCharacters;
+	    		}
+	    		if(finished) {
+	    			textObject.text = startString;
+	    			showTimer.turnOff();
+	    		}
+	    	}
+    	}
+    }
+}
