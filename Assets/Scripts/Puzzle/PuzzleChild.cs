7fd8172393b2b7eb86c18c3e0d789ab768e749c1
Oliver <oliver_marsh@live.com.au>
2019-05-05 21:31:36 +0930

    made damge numbers come p

---
+++ Assets/Scripts/Puzzle/PuzzleChild.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleChild : MonoBehaviour, IHitBox
+{	
+	public PuzzleParent puzzleParent;
+	public int puzzleValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void wasHit(int damage, string type) {
+    	puzzleParent.addValue(puzzleValue);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Puzzle/PuzzleParent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleParent : MonoBehaviour
+{
+	public int[] puzzle;
+	private int[] puzzleSoFar;
+	private int puzzleAt;
+	private int puzzleCount;
+	public Animator animator;
+	public string triggerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        puzzleSoFar = new int[16];
+    }
+
+    public solvePuzzle() {
+    	int indexToStart = puzzleAt + 1;
+    	if(indexToStart >= puzzle.Length) {
+    		indexToStart = 0;
+    	}
+    	for()
+    	animator.SetTrigger(triggerName);
+    }
+
+    public void addValue(int valueToAdd) {
+    	if(puzzleCount < puzzle.Length) {
+    		puzzleCount++;	
+    	}
+    	//wrap running buffer
+    	puzzleAt++;
+    	if(puzzleAt >= puzzle.Length) {
+    		puzzleAt = 0;
+    	}
+
+    	puzzleSoFar[puzzleAt] = valueToAdd;
+    	solvePuzzle();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
